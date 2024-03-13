c252f33623e61f225060899b0185f9c33a69057e
Tomo <tomobratic23@gmail.com>
2024-03-13 13:54:44 +0100

    fetching application settings

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -40,9 +40,9 @@ export async function createEditCabin(newCabin, id) {
     throw new Error("Cabins could not be created");
   }
 
-  // 2. Upload image
   if (hasImagePath) return data;
 
+  // 2. Upload image
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
     .upload(imageName, newCabin.image);
