575de787b0010ea149d83468c6b629af3aec6010
Tomo <tomobratic23@gmail.com>
2024-04-03 14:14:44 +0200

    Building the Bookings Table

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,9 +1,16 @@
-import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import Empty from "../../ui/Empty";
+import { useBookings } from "./useBookings";
+import Spinner from "../../ui/Spinner";
+import BookingRow from "./BookingRow";
 
 function BookingTable() {
-  const bookings = [];
+  const { bookings, isLoading } = useBookings();
+
+  if (!bookings?.length) return <Empty resource="bookings" />;
+
+  if (isLoading) return <Spinner />;
 
   return (
     <Menus>
