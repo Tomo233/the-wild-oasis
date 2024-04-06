df1a0952701733fcd6692e88ac55a184f40a3672
Tomo <tomobratic23@gmail.com>
2024-04-06 18:22:23 +0200

    API-Side Pagination Paginating Bookings

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -3,6 +3,7 @@ import { HiChevronDoubleLeft } from "react-icons/hi";
 import { HiChevronDoubleRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -59,8 +60,6 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
-
-const PAGE_SIZE = 10;
 
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
